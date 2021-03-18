dca64f8f5dd2ebf1d34e786d2c8992a6abc600d5
Frederick Zhu <zhu.yuming@outlook.com>
2021-03-18 15:08:19 -0500

    Committed target

--- target/classes/db/T-create_tables.sql
+++ target/classes/db/T-create_tables.sql
@@ -1,13 +1,14 @@
-CREATE TABLE IF NOT EXISTS user (
-    id          VARCHAR(20),
-    role        ENUM('A', 'D', 'I')
-                                NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS people (
+CREATE TABLE IF NOT EXISTS Instructor (
     cnetid      VARCHAR(20),
-    first_name  VARCHAR(20)     NOT NULL,
-    last_name   VARCHAR(20)     NOT NULL,
+    first_name  VARCHAR(20)             NOT NULL,
+    last_name   VARCHAR(20)             NOT NULL,
     PRIMARY KEY (cnetid)
 );
+
+CREATE TABLE IF NOT EXISTS departments (
+    abbr        CHAR(4),
+    title       VARCHAR(80)             NOT NULL,
+    PRIMARY KEY (abbr)
+);
+
+CREATE TABLE IF NOT EXISTS
