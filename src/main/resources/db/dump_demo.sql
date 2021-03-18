a1885ee8f2deb605fa285229b33af62fb4e37288
Frederick Zhu <zhu.yuming@outlook.com>
2021-03-18 15:54:15 -0500

    Added demo data dump

---
+++ src/main/resources/db/dump_demo.sql
@@ -0,0 +1,16 @@
+INSERT INTO instructor      VALUES ("gbrady", "Geraldline", "Brady"),
+                                   ("achaudhary", "Amitabh", "Chaudhary"),
+                                   ("lsamuel", "Lamont", "Samuels"),
+                                   ("mshacklette", "Mark", "Shacklette");
+
+INSERT INTO department      VALUES ("MPCS", "Masters Program in Computer Science");
+
+INSERT INTO course          VALUES ("MPCS55001", "MPCS", 55001, "Algorithms"),
+                                   ("MPCS52060", "MPCS", 52060, "Parallel Programming"),
+                                   ("MPCS51205", "MPCS", 51205, "Topics in Software Engineering");
+
+INSERT INTO schedule_record VALUES ("MPCS55001-1_2020AUTUMN", "MPCS55001", 2020, "AUTUMN", 1, "gbrady", 1, "TUE", "17:30", "19:00"),
+                                   ("MPCS55001-2_2020AUTUMN", "MPCS55001", 2020, "AUTUMN", 2, "achaudhary", 1, "TUE", "14:40", "16:00"),
+                                   ("MPCS55001-2_2020AUTUMN", "MPCS55001", 2020, "AUTUMN", 2, "achaudhary", 2, "THU", "14:40", "16:00"),
+                                   ("MPCS52060-1_2020AUTUMN", "MPCS52060", 2020, "AUTUMN", 1, "lsamuel", 1, "MON", "17:20", "19:20"),
+                                   ("MPCS51205-1_2020AUTUMN", "MPCS51205", 2020, "AUTUMN", 1, "mshacklette", 1, "MON", "17:00", "20:00");
